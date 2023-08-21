24eeaa2305e4dd7469af0a741f1e5cf4d8c73938
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-08-21 10:03:14 +0200

    priva 2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script >
+import Field from './components/Field.vue';
 
+export default {
+  name: 'App',
+  components: {
+    Field
+  }
+}
 </script>
 
 <template>
   <h1>Ciao mondo</h1>
+  <Field/>
 </template>
 
 <style lang="scss">
---
+++ src/components/Field.vue
@@ -0,0 +1,20 @@
+<script>
+    import { store } from '../store';
+    export default {
+        name: 'Field',
+        data() {
+            return {
+                store
+            }
+        }
+    }
+
+</script>
+
+<template>
+    <div>
+        <h1>
+            prova {{ store.prova }}
+        </h1>
+    </div>
+</template>
