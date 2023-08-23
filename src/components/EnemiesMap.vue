f0a9bdb64eb2dffbf64732e24e58c0b510e17712
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-08-23 12:11:26 +0200

    cambiata icona boss

--- src/components/EnemiesMap.vue
+++ src/components/EnemiesMap.vue
@@ -15,7 +15,7 @@ export default {
         <div class="row justify-content-center w-25 mx-auto">
             <div v-for="(enemy, index) in store.MaxEnemies" :key="index" class="col-1">
                 <div :class="(index+1 == store.ActualEnemies)? '' : 'opacity-25'">
-                    <i class="fa-solid" :class="(index+1 == 10) ? 'fa-skull-crossbones' : 'fa-circle-dot'" style="font-size: 12px;"></i>
+                    <i class="fa-solid" :class="(index+1 == 10 && store.ActualStage == 10) ? 'fa-skull-crossbones' : 'fa-circle-dot'" style="font-size: 12px;"></i>
                 </div>
             </div>
 
