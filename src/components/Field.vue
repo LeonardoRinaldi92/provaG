95a4ca3a57d0cbde3680c8d24ca6673cd570cd66
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-08-24 13:09:59 +0200

    aggiunta commenti funzioni pt.1

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -17,79 +17,134 @@
             }
         },
         methods: {
+            //aumento vita dei mostri
             increaseEnemyLife(){
+                //aumento vita x 8 nel caso sia un Boss di fine stage
                 if (store.ActualEnemies == 9 && store.ActualStage == 9){
                     store.enemyMaxLife *= 8
+                //aumento vita nel caso sia un miniBoss 
                 }else if(store.ActualEnemies == 9 ) {
                     store.enemyMaxLife = store.enemyMaxLife * 4
+                //reset della vita dei nemici dopo boss
                 }else if (store.ActualEnemies == 10 && store.ActualStage == 10 ){
                     store.enemyLife /= 7
                 }
+                //reset della vita dei nemici dopo miniboss ma con aggiunta di un extra 30%
                 else if(store.ActualEnemies == 10){
                     store.enemyMaxLife = store.enemyMaxLife/ 3
+                //aumento classica di vita dopo ogni nemico
                 }else {
                     store.enemyMaxLife*= 1.1
                 }
+                //rendi numero intero vita dei mostri
                 store.enemyMaxLife = Math.round(store.enemyMaxLife)
+                //pareggia vita massima con vita attuale dei nemici
                 store.enemyLife = store.enemyMaxLife
             },
+
+            //danno al click
             hitEnemy(){
+                //check se si è in poszioni dove non si accetta il danno da click
                 if(this.disableClickAtk == false){
+                    //sottrai il valore del danno da click dalla vita dei nemici
                     store.enemyLife  = store.enemyLife - store.clickValue
+                    //richiama la funzione di aggiunta soldi
                     this.addCoin()
                 }
             },
+            //funzione aggiunta soldi
             addCoin() {
+                //se la vita del mostro si azzera
                 if(store.enemyLife <= 0 ) {
+                    //richiama la funzione di aumento della vita
                     this.increaseEnemyLife()
+                    //se sconfiggi un boss di fine stage
                     if(store.ActualEnemies == 10 && store.ActualStage == 10){
-                        store.coin = Math.round((store.coin + store.coinValue) * 5) 
+                        //aggiunge ai soldi il valore fisso di vittoria e moltiplica il tutto x 5
+                        store.coin = Math.round((store.coin + store.coinValue) * 5)
+                        //aumento il valore fisso di vittoria del 50% 
                         store.coinValue*=1.5
                     }
-                    else if(store.ActualEnemies == 10){                       
+                    //se sconfiggi un miniboss
+                    else if(store.ActualEnemies == 10){           
+                        //aggiunge ai soldi il valore fisso di vittoria e moltiplica il tutto x 2            
                         store.coin = Math.round((store.coin + store.coinValue) * 2) 
+                        //aumento il valore fisso di vittoria del 20% 
                         store.coinValue*=1.2
+                    //in tutti gli altri casi
                     }else{
+                        //aumento il valore fisso di vittoria del 5% 
                         store.coinValue*=1.05
                     }
-                    
-                    if (store.ActualStage <= 9) {
-                        if(store.ActualEnemies < 10){
-                            store.ActualEnemies ++
-                        } else {
-                            store.ActualEnemies = 1
-                            store.ActualStage ++
-                        }
-                        if((!store.ActualEnemies == 10 && store.ActualStage == 10) || !(store.ActualEnemies == 10)){
-                            store.coin = Math.round(store.coin + store.coinValue) 
-                        }
-                    }
-                    else {
-                        store.ActualStage = 1
-                        store.AcutalFloor ++
-                        store.timer = 30
-                    } 
+                    //richiama funzione x aumento livello/stage e modifica mappa
+                    this.checkStage()
                 }
             },
+            //funzione x aumento livello/stage e modifica mappa
+            checkStage(){
+                //determiniamo se non siamo al boss
+                if (store.ActualStage <= 9) {
+                    //determiniamo se non siamo al miniboss
+                    if(store.ActualEnemies < 10){
+                        //aumentiamo di 1 la posizione nella mappa
+                        store.ActualEnemies ++
+                    //se siamo al miniboss    
+                    } else {
+                        //reset sulla mappa a 1
+                        store.ActualEnemies = 1
+                        //aumento di 1 dello stage
+                        store.ActualStage ++
+                    }
+
+                    //determiniamo se quello che abbiamo sconfitto è un mostro qualsiasi
+                    if((!store.ActualEnemies == 10 && store.ActualStage == 10) || !(store.ActualEnemies == 10)){
+                        //aggiubgi ai soldi il valore fisso di guadagno
+                        store.coin = Math.round(store.coin + store.coinValue) 
+                    }
+                }
+                //determiniamo se siamo a un boss
+                else {
+                    //reset dello stage a 1
+                    store.ActualStage = 1
+                    //aumenta di 1 il valore del piano
+                    store.AcutalFloor ++
+                    //porta il timer a 30secondi(funzione di check, deprecabile in futuro)
+                    store.timer = 30
+                } 
+            },
+
+            //aumento il valore del danno da click
             increaseClickValue() {
+                //controlla se si dispongono soldi per pagare l'upgrade
                 if (store.coin >= store.clickPrice) {
+                    //sottari i soldi del pagamento
                     store.coin = store.coin - store.clickPrice
+                    //aumenta il livello del click
                     store.clickLevel ++
+                    //aumenta il valore del danno 
                     store.clickValue+= store.increaseClickValue
+                    //determina se l'upgrade è di livello 26/50/75 ecc..
                     if(store.clickLevel % 25 == 0 ) {
+                        //raddoppia il valore di danno del click
                         store.clickValue *= 2
                     }
+                    //determina se l'upgrade è di livello 10,20,30 ecc...
                     else if(store.clickLevel % 10 == 0 ){
+                        //aumenta del 50% il danno del click
                         store.clickValue *= 1.5
                     }
+                    //rendi il valore del danno da click un intero
                     store.clickPrice = Math.round((store.clickPrice * 1.2 ) + (((store.clickPrice * 1.2) / 100) * 10))
-                    
                 }
             },
+
+
             calculateIncreaseDps(ally){
                 let dps =  Math.round(ally.Dps * ally.increaseDps)
                 return dps
             },
+
+            //calcola il prezzo dell'upgrade del danno degli alleati
             calculatePriceToLevelUp(ally){
                 let price = null
                 if(ally.actualLevel == 1){
